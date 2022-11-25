f28c347ce826f5a58a2eeb55515392cf393ef6cf
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2022-11-25 21:43:05 +0200

    add task1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.body,
+};
+
+refs.start.addEventListener('click', onClickBtnChangeColor);
+refs.stop.addEventListener('click', onClickBtnStopChangeColor);
+
+function onClickBtnChangeColor() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.start.setAttribute('disabled', 'disabled');
+  refs.stop.removeAttribute('disabled');
+}
+
+function onClickBtnStopChangeColor() {
+  clearInterval(timerId);
+  refs.start.removeAttribute('disabled');
+  refs.stop.setAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
