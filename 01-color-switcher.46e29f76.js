5f1c09f8e63cd17c1c775a6366f7d37d24c4e18a
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2022-11-25 21:08:14 +0000

    Deploying to gh-pages from @ 28Rostyk/goit-js-hw-09@643417340fe482efb52f0919b21804ef1f4341e1 🚀

--- 01-color-switcher.46e29f76.js
+++
@@ -1,2 +0,0 @@
-const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.body};t.start.addEventListener("click",(function(){timerId=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.start.setAttribute("disabled","disabled"),t.stop.removeAttribute("disabled")})),t.stop.addEventListener("click",(function(){clearInterval(timerId),t.start.removeAttribute("disabled"),t.stop.setAttribute("disabled","disabled")}));
-//# sourceMappingURL=01-color-switcher.46e29f76.js.map
